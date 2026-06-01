1f1a70766329f2f4d71766d6c6205af7505ccfb6
Eleno <elenodoipillon123@gmail.com>
2026-06-01 11:30:10 -0300

    Metodo cadastrar criado. Pronto para testes.

---
+++ src/service/GerenciadorFuncionarios.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class GerenciadorFuncionarios {
+
+ private ArrayList<Funcionario> funcionarios;
+ 
+ public GerenciadorFuncionarios() {
+  funcionarios = new ArrayList<>();
+ }
+ 
+ public void cadastrar(Funcionario funcionario) {
+  funcionarios.add(funcionario);
+ }
+
+ public Funcionario buscarPorCpf(String cpfProcurado) {
+  for (int i = 0; i < funcionarios.size(); i++) {
+   Funcionario f = funcionarios.get(i);
+
+   if (f.getCpf().equals(cpfProcurado)) {
+    return f;
+   }
+  }
+  return null;
+ }
+
+ 
+}
