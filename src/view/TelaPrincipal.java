10af8cae5487347b0eab4d3ce98eab85055edf72
unknown <filipegames2343@gmail.com>
2026-06-02 09:33:13 -0300

    Tela principal ajustada pra não conter a biblioteca do AWT (awt foi usado anteriormente apenas para testes)

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -1,8 +1,7 @@
 package src.view; //nome do package pra importar no Main
 
 import javax.swing.*; //Importa tudo do java swing
-import java.awt.BorderLayout; //importa Border Layout do awt
-import java.awt.GridLayout; //importa Grid Layout do awt
+
 
 public class TelaPrincipal extends JFrame { //Classe TelaPrincipal, o extends JFrame "avisa" a classe que ela vai ser um JFrame (janela)
 
@@ -23,28 +22,32 @@ public class TelaPrincipal extends JFrame { //Classe TelaPrincipal, o extends JF
         abas.addTab("Resumo", painelRelatorio);
         add(abas); 
 
-        painelCadastro.setLayout(new BorderLayout()); //Cria o layout
-    //Novo painel pro formulario, os valores são respectivamente = linha, coluna, gap horizontal e gap vertical
-        JPanel painelFormulario = new JPanel(new GridLayout(7, 2, 10, 10));
+    //Novo painel pro formulario, após isso, eu crio um novo box layout pra alinhar os itens do painel
+        JPanel painelFormulario = new JPanel();
+        painelFormulario.setLayout((new BoxLayout(painelFormulario, BoxLayout.Y_AXIS))); //o y axis é pra tudo ser organizado em coluna
 
 //Campo do nome
         painelFormulario.add(new JLabel("Nome:"));
         JTextField txtNome = new JTextField(20); // o numero 20 reserva espaço na tela para aproximadamente 20 caracteres
+        txtNome.setAlignmentX(LEFT_ALIGNMENT); //ele alinha o texto à esquerda, como se fosse um documento no word
         painelFormulario.add(txtNome);
 
 //Campo do CPF
         painelFormulario.add(new JLabel("CPF:"));
         JTextField txtCpf = new JTextField(11);
+        txtCpf.setAlignmentX(LEFT_ALIGNMENT);
         painelFormulario.add(txtCpf);
 
 //Campo do Cargo        
         painelFormulario.add(new JLabel("Cargo:"));
         JTextField txtCargo = new JTextField(20);
+        txtCargo.setAlignmentX(LEFT_ALIGNMENT);
         painelFormulario.add(txtCargo);
 
 //Campo do Salário
         painelFormulario.add(new JLabel("Salário:"));
         JTextField txtSalario = new JTextField(20);
+        txtSalario.setAlignmentX(LEFT_ALIGNMENT);
         painelFormulario.add(txtSalario);
 
 //Campo Departamento     
@@ -53,12 +56,13 @@ public class TelaPrincipal extends JFrame { //Classe TelaPrincipal, o extends JF
 //Esse combobox cria basicamente uma caixa de seleção de opções, como se fosse um menu hamburguer
         String[] opcoes = {"Selecione o Departamento", "TI", "Financeiro", "Comercial"};
         JComboBox<String> cmbStatus = new JComboBox<>(opcoes);
+        cmbStatus.setAlignmentX(LEFT_ALIGNMENT);
         painelFormulario.add(cmbStatus);
 
-//Radiobutton tipo de contrato organizado em uma linha e 3 colunas
+//Radiobutton tipo de contrato 
 //são botões que você clica pra selecionar o tipo de contrato
         painelFormulario.add(new JLabel("Tipo de Contrato:"));
-        JPanel painelRadios = new JPanel(new GridLayout(1, 3));
+        JPanel painelRadios = new JPanel();
         JRadioButton rbClt = new JRadioButton("CLT");
         JRadioButton rbPj = new JRadioButton("PJ");
         JRadioButton rbEstagio = new JRadioButton("Estágio");
@@ -73,11 +77,12 @@ public class TelaPrincipal extends JFrame { //Classe TelaPrincipal, o extends JF
         painelRadios.add(rbPj);
         painelRadios.add(rbEstagio);
 //Por fim coloca o painelRadios no painelFormulario pra aparecer nele
+        painelRadios.setAlignmentX(LEFT_ALIGNMENT);
         painelFormulario.add(painelRadios);
 
 //Check box de benefícios organizado em uma linha e 4 colunas
         painelFormulario.add(new JLabel("Benefícios:"));
-        JPanel painelChecks = new JPanel(new GridLayout(1, 4));
+        JPanel painelChecks = new JPanel();
         JCheckBox chkVt = new JCheckBox("Vale-transporte");
         JCheckBox chkPs = new JCheckBox("Plano de saúde");
         JCheckBox chkVr = new JCheckBox("Vale-refeição");  
@@ -86,15 +91,18 @@ public class TelaPrincipal extends JFrame { //Classe TelaPrincipal, o extends JF
         painelChecks.add(chkVt);
         painelChecks.add(chkPs);
         painelChecks.add(chkVr);
+        painelChecks.setAlignmentX(LEFT_ALIGNMENT);
         painelFormulario.add(painelChecks);
 
-//Adiciona o painel formulario completo ao painel cadastro, deixando ele centralizado na janela
-        painelCadastro.add(painelFormulario, BorderLayout.CENTER);
-
 //Botão de cadastrar no final, ele tem um addActionListener pra quando eu clicar nele, mostrar uma mensagem no terminal
+
         JButton btnCadastrar = new JButton("Cadastrar");btnCadastrar.addActionListener(e -> {System.out.println("Botão clicado!");});
-        //Adiciona o botão cadastrar embaixo, como um rodapé
-        painelCadastro.add(btnCadastrar, BorderLayout.SOUTH);
+        btnCadastrar.setAlignmentX(LEFT_ALIGNMENT);
+        painelFormulario.add(btnCadastrar);
+
+//Adiciona o painel formulario completo ao painel cadastro
+        painelCadastro.add(painelFormulario);
+
 
         setVisible(true);
     }
