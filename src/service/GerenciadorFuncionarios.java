86a87c58828ad1e7532e0974f9d55b7b6be9634c
Gabriellucas172402 <gabriellucas17062010p.l@gmail.com>
2026-06-09 05:48:41 -0300

    Funções

--- src/service/GerenciadorFuncionarios.java
+++ src/service/GerenciadorFuncionarios.java
@@ -4,73 +4,85 @@ import java.util.ArrayList;
 
 public class GerenciadorFuncionarios {
 
- private ArrayList<Funcionario> funcionarios;
- 
+    private ArrayList<Funcionario> funcionarios;
 
+    public GerenciadorFuncionarios() {
+        funcionarios = new ArrayList<>();
+    }
 
- public GerenciadorFuncionarios() {
-    funcionarios = new ArrayList<>();
- } 
- 
-public boolean cadastrarFuncionario(Funcionario funcionario) {
+    public boolean cadastrarFuncionario(Funcionario funcionario) {
+        if (validarCpf(funcionario.getCpf())) {
+            return false;
+        }
+        funcionarios.add(funcionario);
+        return true;
+    }
 
-    if (validarCpf(funcionario.getCpf())) {
+    public Funcionario buscarPorCpf(String cpfProcurado) {
+        for (int i = 0; i < funcionarios.size(); i++) {
+            Funcionario f = funcionarios.get(i);
+            if (f.getCpf().equals(cpfProcurado)) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Funcionario> listarFuncionarios() {
+        return funcionarios;
+    }
+
+    public ArrayList<Funcionario> filtrarAtivos() {
+        ArrayList<Funcionario> ativos = new ArrayList<>();
+        for (Funcionario f : funcionarios) {
+            if (f.isAtivo()) {
+                ativos.add(f);
+            }
+        }
+        return ativos;
+    }
+
+    public boolean validarCpf(String cpf) {
+        return buscarPorCpf(cpf) != null;
+    }
+
+    public boolean desativarFuncionario(String cpf) {
+        Funcionario funcionario = buscarPorCpf(cpf);
+        if (funcionario != null) {
+            funcionario.setAtivo(false);
+            return true;
+        }
         return false;
     }
 
-    funcionarios.add(funcionario);
-    return true;
-}
+  
 
- public Funcionario buscarPorCpf(String cpfProcurado) {
-
-   for (int i = 0; i < funcionarios.size(); i++) {
-    Funcionario f = funcionarios.get(i);
-
-    if (f.getCpf().equals(cpfProcurado)) {
-     return f;
-    }
-   }
-
-  return null;
-}
-
-public ArrayList<Funcionario> listarFuncionarios() {
-    return funcionarios;
-}
-
-public ArrayList<Funcionario> filtrarAtivos() {
-
-    ArrayList<Funcionario> ativos = new ArrayList<>();
-
-    for (Funcionario f : funcionarios) {
-
-        if (f.isAtivo()) {
-            ativos.add(f);
+    public boolean reativarFuncionario(String cpf) {
+        Funcionario funcionario = buscarPorCpf(cpf);
+        if (funcionario != null) {
+            funcionario.setAtivo(true);
+            return true;
         }
-
+        return false;
     }
 
-    return ativos;
-}
-
-public boolean validarCpf(String cpf) {
-    return buscarPorCpf(cpf) != null;
-}
-
-public boolean desativarFuncionario(String cpf) {
-
-    Funcionario funcionario =
-        buscarPorCpf(cpf);
-
-    if (funcionario != null) {
-
-        funcionario.setAtivo(false);
-        return true;
-
+    public int contarPorDepartamento(String departamento) {
+        int contador = 0;
+        for (Funcionario f : funcionarios) {
+            if (f.isAtivo() && f.getDepartamento().equals(departamento)) {
+                contador++;
+            }
+        }
+        return contador;
     }
 
-    return false;
-}
-
+    public double calcularTotalSalarios() {
+        double total = 0;
+        for (Funcionario f : funcionarios) {
+            if (f.isAtivo()) {
+                total += f.getSalario();
+            }
+        }
+        return total;
+    }
 }
